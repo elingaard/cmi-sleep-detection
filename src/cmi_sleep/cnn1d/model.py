dd7ba3144a8ca5ab871668b050e473aca3c08dc2
Martin Elingaard <m.elingaard@gmail.com>
2023-11-03 11:34:20 +0100

    fix FPN bug where only one layer was used. Add dropout and weight decay to prevent overfitting

--- src/cmi_sleep/cnn1d/model.py
+++ src/cmi_sleep/cnn1d/model.py
@@ -33,8 +33,8 @@ class FPN1D(nn.Module):
 
     def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
         x1 = self.conv1(x)
-        x2 = self.conv1(x)
-        x3 = self.conv1(x)
+        x2 = self.conv2(x)
+        x3 = self.conv3(x)
         x = torch.cat([x1, x2, x3], dim=1)
         x = self.batch_norm(x)
         x = self.activation(x)
@@ -63,19 +63,24 @@ class ResNetBlock1D(nn.Module):
 
 
 class ClassificationHead(nn.Module):
-    def __init__(self, in_chs, kernel_size) -> None:
+    def __init__(self, in_chs) -> None:
         super().__init__()
-        self.conv1 = nn.Conv1d(
-            in_chs, in_chs // 4, kernel_size=kernel_size, padding="same"
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(
+                in_chs, in_chs // 4, kernel_size=5, padding="same"
+            ),
+            nn.BatchNorm1d(in_chs // 4),
+            nn.LeakyReLU(),
+            nn.Dropout1d(p=0.4)
         )
         self.conv2 = nn.Conv1d(
-            in_chs // 4, 1, kernel_size=61, padding="same", dilation=2
+            in_chs // 4, 1, kernel_size=61, padding="same"
         )
 
     def forward(
         self, x: torch.FloatTensor
     ) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
-        x = self.conv1(x).squeeze()
+        x = self.conv1(x)
         y_logits = self.conv2(x).squeeze()
         y_proba = F.sigmoid(y_logits)
         return y_logits, y_proba
@@ -89,17 +94,18 @@ class EventDetectionCNN(nn.Module):
         self.resnet_blocks = nn.Sequential(
             *[ResNetBlock1D(resnet_feat_chs, kernel_size=11)] * n_resnet_blocks
         )
-        self.cls_head = ClassificationHead(resnet_feat_chs, kernel_size=5)
+        self.cls_head = ClassificationHead(resnet_feat_chs)
 
     def forward(self, x):
         x = self.input_layer(x)
         x = self.resnet_blocks(x)
         y_logits, y_proba = self.cls_head(x)
         return y_logits, y_proba
-    
+
     def predict_from_df(self, df, feat_cols):
+        x = df[feat_cols].T.to_numpy(dtype=np.float32)
+        x = torch.tensor(x).float().unsqueeze(0)
         with torch.no_grad():
-            x = torch.tensor(df[feat_cols].T.to_numpy()).float().unsqueeze(0)
             _, y_proba = self.forward(x)
         return y_proba.numpy()
 
@@ -135,4 +141,4 @@ class CMISleepDetectionCNN(pl.LightningModule):
         return val_loss, val_acc
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.model.parameters(), lr=2e-4)
+        return torch.optim.AdamW(self.model.parameters(), lr=2e-4)

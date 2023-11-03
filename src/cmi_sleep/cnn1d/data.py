2133e36162bcc5c1dd7c42ecb47fd71cc6ae90a6
Martin Elingaard <m.elingaard@gmail.com>
2023-11-03 11:33:29 +0100

    use pyarrow data types and change data sampling to use time step split dataframes

--- src/cmi_sleep/cnn1d/data.py
+++ src/cmi_sleep/cnn1d/data.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import pandas as pd
 import numpy as np
@@ -19,18 +19,18 @@ def time_encoding(ts_df: pd.DataFrame):
     time_df["onset_prior"] = (
         (ts_df.timestamp.dt.hour * 60 + ts_df.timestamp.dt.minute)
         .map(onset_prior_dict)
-        .astype(np.float32)
+        .astype("float[pyarrow]")
     )
     time_df["awake_prior"] = (
         (ts_df.timestamp.dt.hour * 60 + ts_df.timestamp.dt.minute)
         .map(awake_prior_dict)
-        .astype(np.float32)
+        .astype("float[pyarrow]")
     )
     return time_df
 
 
 class CMITimeSeriesSampler(Dataset):
-    """Dataset class for sampling time-series from the CMI Sleep Detection dataset. 
+    """Dataset class for sampling time-series from the CMI Sleep Detection dataset.
     A random time-series of size 'sample_size' is sampled from the specified series
     index"""
 
@@ -45,29 +45,42 @@ class CMITimeSeriesSampler(Dataset):
         self.series_ids = list(self.series_grps.groups.keys())
         self.sample_size = sample_size
 
-    def check_timeseries_continuity(self, ts_df):
-        step_sizes = ts_df["step"].diff()[1:].astype(int)
-        is_cont = (step_sizes == step_sizes.iloc[0]).all()
-        return is_cont
+    def split_df_on_stepsize(self, sample_df) -> List[pd.DataFrame]:
+        step_sizes = sample_df["step"].diff()[1:]
+        split_idxs = np.nonzero(step_sizes != 12)[0] + 1
+        if len(split_idxs) > 0:
+            split_dfs = np.split(sample_df, split_idxs)
+        else:
+            split_dfs = [sample_df]
+
+        return split_dfs
 
     def __len__(self):
         return len(self.series_ids)
 
     def __getitem__(self, index):
         sid = self.series_ids[index]
-        sample_df = self.series_grps.get_group(sid)
+        series_df = self.series_grps.get_group(sid)
+        series_df = series_df.reset_index(drop=True)
+        split_dfs = self.split_df_on_stepsize(series_df)
+
+        if len(split_dfs) > 1:
+            df_lens = np.array([len(df) for df in split_dfs])
+            sample_probs = df_lens / np.sum(df_lens)
+            sample_idx = np.random.choice(range(len(split_dfs)), p=sample_probs)
+        else:
+            sample_idx = 0
+        sample_df = split_dfs[sample_idx]
         sample_df = sample_df.reset_index(drop=True)
+
         assert len(sample_df) > self.sample_size
 
-        is_cont_ts = False
-        while ~is_cont_ts:
-            start_idx = np.random.randint(0, len(sample_df) - self.sample_size)
-            end_idx = start_idx + self.sample_size
-            ts_df = sample_df[start_idx:end_idx]
-            is_cont_ts = self.check_timeseries_continuity(ts_df)
+        start_idx = np.random.randint(0, len(sample_df) - self.sample_size)
+        end_idx = start_idx + self.sample_size
+        ts_df = sample_df[start_idx:end_idx]
 
-        X_data = ts_df[self.feat_cols].T.to_numpy()
-        y_data = ts_df[self.target_col].to_numpy().astype(np.float32)
+        X_data = ts_df[self.feat_cols].T.to_numpy(dtype=np.float32)
+        y_data = ts_df[self.target_col].to_numpy(dtype=np.float32)
 
         return X_data, y_data
 
@@ -88,7 +101,7 @@ class CMIDataModule(pl.LightningDataModule):
         feat_cols = ["anglez_1min_mean", "enmo_1min_mean", "onset_prior", "awake_prior"]
         target_col = "asleep"
         dset = CMITimeSeriesSampler(
-            series_df,
+            self.series_df,
             sample_size=self.sample_size,
             feat_cols=feat_cols,
             target_col=target_col,
